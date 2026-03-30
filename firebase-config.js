74110d70b45b96115797238d895513cf6c86bf8d
anchorrosefreedom <anchorrosefreedom@github.com>
2026-03-30 14:09:23 -0800

    Add Firebase Auth login/register pages

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase config for CrowdSourced Cooking
+const firebaseConfig = {
+  apiKey: "AIzaSyBk3w4fTG2RZGBJ7VWZg1kKvPBc5bqWlzk",
+  authDomain: "crowdsourcedcooking.firebaseapp.com",
+  projectId: "crowdsourcedcooking",
+  storageBucket: "crowdsourcedcooking.firebasestorage.app",
+  messagingSenderId: "1025589287082",
+  appId: "1:1025589287082:web:15569e323aa2781ab858d3"
+};
