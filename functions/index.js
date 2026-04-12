86de592eecbf13908d72b8b972aa11bc16ab23fd
anchorrosefreedom <anchorrosefreedom@github.com>
2026-04-12 15:05:08 -0800

    Fix author object

--- functions/index.js
+++ functions/index.js
@@ -80,20 +80,14 @@ exports.saveRecipe = functions.https.onRequest((req, res) => {
   }
   
   // Always use domain as author
-  let author = '';
+  let author = data.author;
   if (data.url) {
     try {
       const u = new URL(data.url);
       author = u.hostname.replace('www.', '');
     } catch(e) {}
   }
-  data.author = ''; // Will be overwritten by domain below
-  // Always use domain from sourceUrl if available
-  data.author = data.sourceUrl ? new URL(data.sourceUrl).hostname.replace('www.', '') : (data.author || 'Anonymous');
-    try {
-      data.author = new URL(data.sourceUrl).hostname.replace('www.', '');
-    } catch(e) {}
-  }
+  data.author = author;
   if (!data || !data.title) {
     res.status(400).json({error: "Missing title"});
     return;
@@ -193,8 +187,7 @@ exports.importRecipe = functions.https.onRequest((req, res) => {
             description: (recipe.description || '').trim(),
             ingredients: ingredients,
             instructions: instructions,
-            image: (Array.isArray(recipe.image) ? recipe.image[0] : recipe.image) || '',
-            sourceUrl: url,
+            image: (Array.isArray(recipe.image) ? recipe.image[0] : recipe.image) || ''
           }
         });
         return;
